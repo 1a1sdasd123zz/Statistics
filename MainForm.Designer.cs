de1947498a0e0f852e4941163a1a8741bb5956ff
2399423548 <2399423548@qq.com>
2025-06-18 19:21:11 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Forms;
+
+namespace Statistics
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shiftTabControl = new System.Windows.Forms.TabControl();
+            this.incrementButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // shiftTabControl 设置
+            this.shiftTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.shiftTabControl.Location = new System.Drawing.Point(0, 40);
+            this.shiftTabControl.Name = "shiftTabControl";
+            this.shiftTabControl.SelectedIndex = 0;
+            this.shiftTabControl.Size = new System.Drawing.Size(800, 410);
+            this.shiftTabControl.TabIndex = 0;
+
+            // incrementButton 设置
+            this.incrementButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.incrementButton.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.incrementButton.Location = new System.Drawing.Point(0, 0);
+            this.incrementButton.Name = "incrementButton";
+            this.incrementButton.Size = new System.Drawing.Size(800, 40);
+            this.incrementButton.TabIndex = 1;
+            this.incrementButton.Text = "增加当前小时数据";
+            this.incrementButton.UseVisualStyleBackColor = true;
+            this.incrementButton.Click += new System.EventHandler(this.IncrementButton_Click);
+
+            // MainForm 设置
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shiftTabControl);
+            this.Controls.Add(this.incrementButton);
+            this.Name = "MainForm";
+            this.Text = "数据统计系统";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl shiftTabControl;
+        private StatisticsControl dayShiftControl;
+        private StatisticsControl nightShiftControl;
+        private Button incrementButton;
+    }
+}
---
+++ StatisticsControl.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Statistics
+{
+    partial class StatisticsControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button clearButton; // 添加清零按钮成员变量
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hourChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button(); // 初始化清零按钮
+            ((System.ComponentModel.ISupportInitialize)(this.hourChart)).BeginInit();
+            this.SuspendLayout();
+
+            // hourChart 设置
+            this.hourChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hourChart.Location = new System.Drawing.Point(0, 70);
+            this.hourChart.Name = "hourChart";
+            this.hourChart.Size = new System.Drawing.Size(600, 280);
+            this.hourChart.TabIndex = 0;
+
+            // totalLabel 设置
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.totalLabel.Location = new System.Drawing.Point(0, 0);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Padding = new System.Windows.Forms.Padding(5);
+            this.totalLabel.Size = new System.Drawing.Size(113, 29);
+            this.totalLabel.TabIndex = 1;
+            this.totalLabel.Text = "总量: 0";
+
+            // clearButton 设置
+            this.clearButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clearButton.Location = new System.Drawing.Point(0, 30);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(600, 40);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "清零当前班次数据";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+
+            // StatisticsControl 设置
+            this.Controls.Add(this.hourChart);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.totalLabel);
+            this.Name = "StatisticsControl";
+            this.Size = new System.Drawing.Size(600, 350);
+            ((System.ComponentModel.ISupportInitialize)(this.hourChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            InitializeChart();
+        }
+
+        #endregion
+    }
+}
